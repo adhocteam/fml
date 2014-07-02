ad67a46dc2115182ad3e0b5d2b6fd916139df56f
Bill Mill <bill.mill@gmail.com>
2014-07-02 09:36:52 -0400

    add a simple spec

---
+++ spec/form_spec.rb
@@ -0,0 +1,22 @@
+# bowling_spec.rb
+require 'dttforms'
+
+describe "Basic form handling" do
+  it "creates a form from a FML spec" do
+    #TODO stick this in a feile
+    form = <<-eos
+form:
+  title: "Diabetes mellitus evaluation"
+  fieldsets:
+    - fieldset:
+      - field:
+          name: "hasDiabetes"
+          fieldType: "yes_no"
+          label: "Does the patient currently have diabetes mellitus?"
+          isRequired: true
+    eos
+
+    f = DTTForm.new(form)
+    expect(f.title).to eq "Diabetes mellitus evaluation"
+  end
+end
