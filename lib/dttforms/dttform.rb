12f23ca9ab701dff8fc266447d3bef205377c424
Bill Mill <bill.mill@gmail.com>
2014-07-02 10:51:15 -0400

    make fields kind of work

--- lib/dttforms/dttform.rb
+++ lib/dttforms/dttform.rb
@@ -1,17 +1,58 @@
 require 'yaml'
 
+class DTTField
+  attr_reader :name, :type, :label, :prompt, :required, :options,
+    :conditional_on, :validations
+
+  def initialize(name, type, label, prompt, required, options, conditional_on,
+                validations)
+    @name = name
+    @type = type
+    @label = label
+    @prompt = prompt
+    @required = required
+    @options = options
+    @conditional_on = conditional_on
+    @validations = validations
+  end
+end
+
 class DTTForm
   attr_reader :form, :title, :fieldsets
 
   def initialize(form)
-    yaml = YAML.load(form)
-    @form = getrequired(yaml, "form")
-    @title = getrequired(@form, "title")
-    @fieldsets = getrequired(@form, "fieldsets")
-    #TODO make fieldset objs
+    parse(YAML.load(form))
   end
 
   private
+
+  def parse(yaml)
+    @form = getrequired(yaml, "form")
+    @title = getrequired(@form, "title")
+
+    # @fieldsets is just a list of lists of fields
+    @fieldsets = getrequired(@form, "fieldsets").collect do |fieldset|
+      getrequired(fieldset, "fieldset").collect do |field|
+        parsefield(field["field"])
+      end
+    end
+  end
+
+  def parsefield(field)
+    name = getrequired(field, "name")
+    type = getrequired(field, "fieldType")
+    label = getrequired(field, "label")
+    prompt = field["prompt"]
+    is_required = field["isRequired"]
+
+    #TODO: actually handle the things below this point
+    options = field["options"]
+    conditional = field["conditionalOn"]
+    validations = field["validations"]
+
+    DTTField.new(name, type, label, prompt, is_required, options, conditional,
+                 validations)
+  end
 
   def getrequired(obj, attr)
     x = obj[attr]
--- spec/form_spec.rb
+++ spec/form_spec.rb
@@ -1,4 +1,3 @@
-# bowling_spec.rb
 require 'dttforms'
 
 describe "Basic form handling" do
@@ -12,12 +11,20 @@ form:
       - field:
           name: "hasDiabetes"
           fieldType: "yes_no"
-          label: "Does the patient currently have diabetes mellitus?"
+          label: "bananarama"
           isRequired: true
     eos
 
     f = DTTForm.new(form)
     expect(f.title).to eq "Diabetes mellitus evaluation"
     expect(f.form).to eq YAML.load(form)["form"]
+    expect(f.fieldsets.length).to eq 1
+    expect(f.fieldsets[0].length).to eq 1
+
+    field = f.fieldsets[0][0]
+    expect(field.name).to eq "hasDiabetes"
+    expect(field.type).to eq "yes_no"
+    expect(field.label).to eq "bananarama"
+    expect(field.required).to eq true
   end
 end
