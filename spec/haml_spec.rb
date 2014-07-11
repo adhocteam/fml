17cc9a9362982c351a36cb967568d6f504cc61f9
Bill Mill <bill.mill@gmail.com>
2014-07-11 16:56:33 -0400

    update tests for lack of railsform

--- spec/haml_spec.rb
+++ spec/haml_spec.rb
@@ -1,14 +1,5 @@
 require 'fml'
 
-class Formmock
-  def label(name, label)
-    "<label for=\"#{name}\">#{label}</label>"
-  end
-  def radio_button(name, val, options)
-    "<input type=\"radio\">#{val}</input>"
-  end
-end
-
 describe "HAML generation" do
   it "creates a HAML from a FMLForm" do
     form = File.read(File.join(File.dirname(__FILE__), "data", "simple.yaml"))
@@ -16,7 +7,7 @@ describe "HAML generation" do
     f = FML::FMLForm.new(form)
 
     h = FML::HamlAdapter.new(f)
-    output = h.render(Formmock.new)
+    output = h.render()
     expected = File.read(File.join(File.dirname(__FILE__), "data", "simple.html"))
     expect(output).to eq expected
   end
