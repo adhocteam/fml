754be5a6112e030bdee8f8065c4417037841aaa8
Bill Mill <bill.mill@gmail.com>
2014-07-14 16:51:41 -0400

    test

--- spec/form_spec.rb
+++ spec/form_spec.rb
@@ -43,10 +43,31 @@ describe FML::FMLForm do
     expect(obj["form"]["fieldsets"].length).to eq 1
     expect(obj["form"]["fieldsets"][0].length).to eq 1
 
-    field = obj["form"]["fieldsets"][0][0]
+    field = obj["form"]["fieldsets"][0]["fieldset"][0]["field"]
     expect(field["name"]).to eq "hasDiabetes"
-    expect(field["type"]).to eq "yes_no"
+    expect(field["fieldType"]).to eq "yes_no"
     expect(field["label"]).to eq "bananarama"
-    expect(field["required"]).to eq true
+    expect(field["isRequired"]).to eq true
+    expect(field["value"]).to eq "true"
+  end
+
+  it "can load itself from json" do
+    form = File.read(File.join(File.dirname(__FILE__), "data", "simple.yaml"))
+
+    params = {hasDiabetes: "true"}
+    form = FML::FMLForm.new(form).fill(params)
+    json = form.to_json
+
+    f = FML::FMLForm.from_json(json)
+    expect(f.title).to eq "Diabetes mellitus evaluation"
+    expect(f.version).to eq "1.0"
+    expect(f.fieldsets.length).to eq 1
+    expect(f.fieldsets[0].length).to eq 1
+
+    field = f.fieldsets[0][0]
+    expect(field.name).to eq "hasDiabetes"
+    expect(field.type).to eq "yes_no"
+    expect(field.label).to eq "bananarama"
+    expect(field.required).to eq true
   end
 end
