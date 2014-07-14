a03787e2aa4952899f05c376edecd1a49fa25d4a
Bill Mill <bill.mill@gmail.com>
2014-07-14 17:11:20 -0400

    test render_show method

--- spec/haml_spec.rb
+++ spec/haml_spec.rb
@@ -1,14 +1,21 @@
 require 'fml'
 
-describe "HAML generation" do
-  it "creates a HAML from a FMLForm" do
+describe FML::HamlAdapter do
+  it "renders an FMLForm into form fields" do
     form = File.read(File.join(File.dirname(__FILE__), "data", "simple.yaml"))
-
     f = FML::FMLForm.new(form)
 
-    h = FML::HamlAdapter.new(f)
-    output = h.render()
+    output = FML::HamlAdapter.new(f).render()
     expected = File.read(File.join(File.dirname(__FILE__), "data", "simple.html"))
+    expect(output).to eq expected
+  end
+
+  it "renders an FMLForm into a show page" do
+    form = File.read(File.join(File.dirname(__FILE__), "data", "simple.yaml"))
+    f = FML::FMLForm.new(form)
+
+    output = FML::HamlAdapter.new(f).render_show()
+    expected = File.read(File.join(File.dirname(__FILE__), "data", "simple_show.html"))
     expect(output).to eq expected
   end
 end
