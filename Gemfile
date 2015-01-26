38b0ab142d5d23033b8e7ce663efe5a37c2f2c10
Bill Mill <bill.mill@gmail.com>
2015-01-26 14:06:15 -0500

    add simplecov coverage reporting

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ group :development do
   gem "rspec"
   gem "byebug", "~> 3.1"
   gem "pry"
+  gem 'simplecov', :require => false
 end
 
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require 'fml_forms'
 require 'byebug'
 
+# Add coverage reporting
+require 'simplecov'
+SimpleCov.start
+
 def getdata(name)
   File.read(File.join(File.dirname(__FILE__), "data", name))
 end
